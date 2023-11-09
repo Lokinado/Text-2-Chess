d4e5580bf4c3d59ea2737b8a4f174e01c2bb6012
Krzysztof Borowski <hisboak@gmail.com>
2023-11-09 20:42:29 +0100

    Added utility to convert text to base 55 number

---
+++ render.js
@@ -0,0 +1,40 @@
+function ClearCanvas() {
+  const canvas = document.getElementById("canvas");
+  const context = canvas.getContext("2d");
+  context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function GetText() {
+  let text = document.getElementById("input").value;
+  return text
+}
+
+function ConvertTextToBase10(text){
+  //This function assumes that input is in base 55 as if characters were digits of a number
+
+  //This function only supports following characters
+  //ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz., and space
+
+  let ret = 0;
+  const lookup = [' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '.', ',']
+  const values = [ ]
+
+  for(let i = text.length - 1; i >= 0 ; i--){
+    const letter = text[i];
+    const exponent = (text.length - i) - 1;
+
+    const coeff = lookup.indexOf(letter)
+    console.log(i, coeff, exponent, coeff * (55 ** exponent))
+    ret += coeff * (55 ** exponent);
+  }
+
+  return ret;
+}
+
+function ConvertNumberToBase13(input){
+  return input.toString(13)
+}
+
+async function Enocode() {
+  let input 
+}
