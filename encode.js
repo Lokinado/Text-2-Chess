c5eda2bccb6a57efa43d7109005ae6dc1ad7f67c
Krzysztof Borowski <hisboak@gmail.com>
2023-11-09 23:56:45 +0100

    Fixed minor bugs and added download button

--- encode.js
+++ encode.js
@@ -22,6 +22,9 @@ function ConvertTextToBase10(text){
 
   let ret = 0;
   const lookup = [' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '.', ',']
+  //Setting the space as the first character of dictionary removes spaces on the beggining of text
+  //This function utilises the fact that leading zeros arent represented as a number
+
 
   for(let i = text.length - 1; i >= 0 ; i--){
     const letter = text[i]
@@ -124,6 +127,12 @@ async function Encode() {
     }
   }
 
+  document.getElementById("download").setAttribute("style", "")
+}
 
-
+var download = function(){
+  var link = document.createElement('a');
+  link.download = 'chessboard.png';
+  link.href = document.getElementById('canvas').toDataURL()
+  link.click();
 }
